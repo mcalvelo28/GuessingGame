ebc91afcd53709014125147f335e09df2e43b42a
Mark Calvelo <mcalvelo28@gmail.com>
2023-08-20 17:32:25 -0400

    Add files via upload

--- ShoppingList/list.js
+++ ShoppingList/list.js
@@ -2,6 +2,11 @@ const button = document.getElementsByClassName("input-button")[0]
 const inputbox = document.getElementsByClassName("input-box")[0]
 const listdiv = document.getElementsByClassName("list-item")[0]
 
+for (i=0; i < localStorage.length; i++) {
+    inputbox.value = localStorage.key(i)
+    createitem()
+}
+
 function createitem () {
     const itemvalue = inputbox.value
     const listitem = document.createElement("li")
@@ -16,8 +21,11 @@ function createitem () {
     listitem.appendChild(spanitem)
 
     listdiv.appendChild(listitem)
+    localStorage.setItem(`${itemvalue}`, '')
+    console.log(localStorage)
 
     function deleteitem() {
+        localStorage.removeItem(`${itemvalue}`)
         listitem.remove()
     }
 
